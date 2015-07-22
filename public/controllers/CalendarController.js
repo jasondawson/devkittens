d42ba9f16db23d6993cfda43f6198cdbd5eccb10
Daniel Falabella <dnlfala@gmail.com>
2015-07-21 23:56:33 -0600

    Improved styling on calendar view.

--- public/controllers/CalendarController.js
+++ public/controllers/CalendarController.js
@@ -3,5 +3,6 @@ angular.module('devKittens')
 .controller('CalendarController', function ($scope, calendarService) {
 	
 	$scope.events = calendarService.get();
+	console.log($scope.events);
 
 });
--- public/services/calendarService.js
+++ public/services/calendarService.js
@@ -7,12 +7,42 @@ angular.module('devKittens')
 	// Temp data
 	var fakeCalendar = [
 		{
+			_id: 'asdaskdja21312',
+			day: 1,
 			topic: 'Orientation - Git',
 			date: new Date(),
 			lessonPlan: 'https://docs.google.com/a/devmounta.in/document/d/1d',
 			projects:  'https://github.com/DevMountain/learn-git',
 			instructor: 'Tyler McGinnis'
-
+		},
+		{
+			_id: '12312qweq3',
+			day: 2,
+			date: new Date('2015-07-23')
+		},
+		{
+			_id: 'asodias9d90',
+			day: 3,
+			topic: 'Orientation - Git',
+			date: new Date('2015-07-24'),
+			lessonPlan: 'https://docs.google.com/a/devmounta.in/document/d/1d',
+			projects:  'https://github.com/DevMountain/learn-git',
+			instructor: 'Tyler McGinnis'
+		},
+		{
+			_id: '12312qweq3',
+			day: 4,
+			date: new Date('2015-07-25')
+		},
+		{
+			_id: '12312qweq3',
+			day: 4,
+			date: new Date('2015-07-26')
+		},
+		{
+			_id: '12312qweq3',
+			day: 4,
+			date: new Date('2015-07-27')
 		}
 	]
 	////////////
