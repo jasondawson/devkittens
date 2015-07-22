425ae7ff4397b178b181f12772ab991f60bce29a
Daniel Falabella <dnlfala@gmail.com>
2015-07-22 02:33:30 -0600

    Scrolling to the top on page load.

--- public/controllers/CalendarController.js
+++ public/controllers/CalendarController.js
@@ -1,7 +1,10 @@
 angular.module('devKittens')
 
 .controller('CalendarController', function ($scope, calendarService) {
-	
+	// TODO: make this a directive
+	document.body.scrollTop = document.documentElement.scrollTop = 0;
+
+
 	// get active course info
 	$scope.course = {
 		name: 'Full Time Web, Summer 2014'
