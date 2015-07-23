791ffb4210ce53b95efaeb9aec0ba908cf76ef8a
Mark McIver <markmciver83@gmail.com>
2015-07-22 21:35:29 -0600

    something

--- public/controllers/CurriculumController.js
+++ public/controllers/CurriculumController.js
@@ -1,1 +1,13 @@
-angular.module('devKittens')
+angular.module('devKittens').controller('CalendarController', function ($scope, calendarService) {
+	// TODO: make this a directive
+	document.body.scrollTop = document.documentElement.scrollTop = 0;
+
+	// get active course info
+	$scope.course = {
+		name: 'Full Time Web, Summer 2014'
+	}
+
+	$scope.events = calendarService.get();
+	// console.log($scope.events);
+
+});
