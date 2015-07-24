f1204a119d480faa810da4106e8f661f322c20c7
Daniel Falabella <dnlfala@gmail.com>
2015-07-24 04:15:34 -0600

    Worked some on the dashboard page. Modals are still broken, and cohorts cards are a bit off in sizing.

--- public/controllers/DashboardController.js
+++ public/controllers/DashboardController.js
@@ -2,9 +2,13 @@ angular.module('devKittens')
 
 .controller('DashboardController', function($scope, $location, cohortData, courseData, courseServices) {
 
+	// Init
 	$scope.toggleAddCohort = false;
-
+	$scope.toggleViewToCohorts = false;
+	$scope.toggleViewToCourses = true;
 	$scope.cohortArray = cohortData;
+
+
 
 	$scope.addCohortView = function() {
 		$scope.toggleAddCohort = !$scope.toggleAddCohort;
@@ -21,17 +25,17 @@ angular.module('devKittens')
 
 
 
-	//toggling between dashboard views
-
-	$scope.toggleViewToCohorts = false;
-	$scope.toggleViewToCourses = false;
-
+	// Toggling between dashboard views
 	$scope.cohortsToggle = function() {
-		$scope.toggleViewToCohorts = !$scope.toggleViewToCohorts;
+		if ($scope.toggleViewToCohorts === true) return;
+
 		$scope.toggleViewToCourses = false;
+		$scope.toggleViewToCohorts = !$scope.toggleViewToCohorts;
 	}
 
 	$scope.coursesToggle = function() {
+		if ($scope.toggleViewToCourses === true) return;
+
 		$scope.toggleViewToCohorts = false;
 		$scope.toggleViewToCourses = !$scope.toggleViewToCourses;
 	}
@@ -39,7 +43,7 @@ angular.module('devKittens')
 
 
 
-	// all that biz for creating a new Course, etc.
+	// All that biz for creating a new Course, etc.
 
 	$scope.createNewCourse = function(obj) {
 		courseServices.createNewCourse(obj)
