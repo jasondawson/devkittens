3b00294850a5f2720fddcca37b2a3a3064348a41
Kyle Zollinger <zollingerkj@gmail.com>
2015-07-23 18:07:28 -0600

    Parsing data before pushing to course schema.

--- controllers/CourseController.js
+++ controllers/CourseController.js
@@ -2,7 +2,11 @@ var Lesson = require('../models/LessonModel.js'),
 	Course = require('../models/CourseModel.js');
 
 exports.createNewCourse = function(req, res) {
-	new Course({title: req.body.title, courseLength: req.body.courseLength})
+	var daysArray = [];
+	for (var i = 1; i <= req.body.courseLength; i++) {
+		daysArray.push({day: i});
+	}
+	new Course({title: req.body.title, courseLength: req.body.courseLength, curriculum: daysArray})
 	.save(function(err, data) {
 		if (err) {
 			res.status(500).json(err);
