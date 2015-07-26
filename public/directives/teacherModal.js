aa0279a7999561832113fece16178055c06328c0
Joseph Kobrosky <jkobrosky@gmail.com>
2015-07-25 20:33:45 -0600

    Finished the instructor section

--- public/directives/teacherModal.js
+++ public/directives/teacherModal.js
@@ -12,11 +12,57 @@ angular.module('devKittens')
 			currentTeacher: '='
 		},
 		controller: function($scope) {
-			$scope.studentSection = false;
+			// $scope.studentSection = false;
 
-			$scope.showStudents = function() {
-				$scope.studentSection = !$scope.studentSection;
-			};
+			// $scope.showStudents = function() {
+			// 	$scope.studentSection = !$scope.studentSection;
+			// };
+
+			$scope.toggleAddMentos = false;
+			$scope.toggleAddSchedule = false;
+
+			$scope.toggleViewToMentos = false;
+			$scope.toggleViewToSchedule = false;
+			$scope.toggleViewToCohort = false;
+
+			$scope.activateMentos = function() {
+				console.log('activateMentos')
+				$scope.activeTab = 'mentos';
+				$scope.toggleViewToMentos = true;
+
+				$scope.toggleViewToCohort = false;	
+				$scope.toggleViewToSchedule = false;
+			}
+
+			$scope.activateSchedule = function() {
+				console.log('activateSchedule')
+				$scope.activeTab = 'schedule';
+				$scope.toggleViewToSchedule = true;
+
+				$scope.toggleViewToCohort = false;
+				$scope.toggleViewToMentos = false;
+			}
+
+			$scope.activateCohort = function() {
+				console.log('activateCohort')
+				$scope.activeTab = 'cohort';
+				$scope.toggleViewToCohort = true;
+
+				$scope.toggleViewToMentos = false;
+				$scope.toggleViewToSchedule = false;
+			}
+
+			$scope.addMentosView = function() {
+				$scope.toggleAddMentos = !$scope.toggleAddMentos;
+			}
+
+			$scope.addScheduleView = function() {
+				$scope.toggleAddSchedule = !$scope.toggleAddSchedule;
+			}
+
+			$scope.addCohortView = function() {
+				$scope.toggleAddCohort = !$scope.toggleAddCohort;
+			}
 
 		}
 	}
