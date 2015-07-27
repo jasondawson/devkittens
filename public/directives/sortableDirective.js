5667df5920f5e26f7d54f2ff8f59e1b41131d837
Daniel Falabella <dnlfala@gmail.com>
2015-07-27 16:49:31 -0600

    Temp push to switch branch.

---
+++ public/directives/sortableDirective.js
@@ -0,0 +1,29 @@
+angular.module('devKittens')
+
+.directive('sortableDirective', function () {
+	return {
+		restrict: 'E',
+		link: function (scope, elem, attrs) {
+
+		$( elem ).sortable({
+			connectWith: ".sortable",
+			handle: ".day",
+			cancel: ".day",
+			placeholder: "portlet-placeholder ui-corner-all"
+		});
+
+		$( ".portlet" )
+		.addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
+		.find( ".portlet-header" )
+		.addClass( "ui-widget-header ui-corner-all" )
+		.prepend( "<span class='ui-icon ui-icon-minusthick portlet-toggle'></span>");
+
+		$( ".portlet-toggle" ).click(function() {
+			var icon = $( this );
+			icon.toggleClass( "ui-icon-minusthick ui-icon-plusthick" );
+			icon.closest( ".portlet" ).find( ".portlet-content" ).toggle();
+		});
+
+		}
+	}
+})
