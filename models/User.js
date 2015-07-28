40250313cbed4364ff9c7b016adecf748b7ead89
Kyle Zollinger <zollingerkj@gmail.com>
2015-07-27 18:02:00 -0600

    github Url

--- models/User.js
+++ models/User.js
@@ -8,32 +8,8 @@ var UserSchema = mongoose.Schema({
 	  }
 	, name: String
 	, avatar: String
-	, github: String
-	// , permissions: {
-	// 	  isAdmin: { type: Boolean, default: false }
-	// 	, isMentor: { type: Boolean, default: false }
-	// 	, isInstructor: { type: Boolean, default: false }
-	// 	, isStudent: {
-	// 		  status: { type: Boolean, default: true }
-	// 		, cohortId: String
-	// 	}
-	// }
-	// // mentor specific
-	// , mentorData: {
-	// 	mentos: [{ type: mongoose.Schema.Types.ObjectId, ref: 'UserSchema' }],
-	//   }
-	// , mentorType: String
-	// , seniority: Number
-	// , cohort: String
+	, githubUrl: String
 	, userType: {type: mongoose.Schema.Types.ObjectId, ref: {type: String, enum: ["Mentor", "Instructor", "Student", "Admin"], default: "Student"}}
-
-	// TODO: Enable github info
-	// , github: {
-	// 	  userName: String
-	// 	, img: String
-	// 	, email: String
-	// }	
-
 })
 
 
@@ -49,21 +25,3 @@ UserSchema.methods.validPassword = function(password) {
 
 // exporting model
 module.exports = mongoose.model('User', UserSchema);
-
-// {
-//     "local": {
-//         "email": "yoda@gmail.com",
-//         "password": "theforce"
-//     },
-//     "name": "Master Yoda",
-//     "avatar": "http://www.magnifiedview.com/wp-content/uploads/2013/01/yoda-Avatar-3.jpg",
-//     "permissions": {
-//         "isAdmin": "true"
-//     },
-//     "mentorData": {
-//         "mentos": "123412341234"
-//     },
-//     "mentorType": "Master Mentor",
-//     "seniority": "5 months",
-//     "cohort": "Full Immersive Web DM4"
-// }
