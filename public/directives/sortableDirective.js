4cff9af0a5527099f4ad6044f83796057f3dff75
Daniel Falabella <dnlfala@gmail.com>
2015-07-29 23:16:08 -0600

    Making front-end sortable (draggable) work on course page.

--- public/directives/sortableDirective.js
+++ public/directives/sortableDirective.js
@@ -16,22 +16,23 @@ angular.module('devKittens')
 		      	$(ui.item).removeClass('active-dragging');
 		      },
 		      update: function (event, ui) {
-		      	var days = elem.find('.day');
-		      	var days = Array.prototype.slice.call(days);
+		      	// console.warn('update ui ', ui);
+		      	// var days = elem.find('.day');
+		      	// var days = Array.prototype.slice.call(days);
 
-		      	var udpatedPositions = days.map(function (day) {
-		      		return day.id.substring(3);
-		      	})
+		      	// var udpatedPositions = days.map(function (day) {
+		      	// 	return day.id.substring(3);
+		      	// })
 
-		      	var location = $location.$$url.split('/')[1];
+		      	// var location = $location.$$url.split('/')[1];
 
-		      	cohortServices.updateCoursesOrder(udpatedPositions, infoStorage.serveCalendarId(), location)
-		      	.then(function (response) {
-		      		// console.info(response);
-		      	})
-		      	.catch(function (err) {
-		      		console.error(err);
-		      	});
+		      	// cohortServices.updateCoursesOrder(udpatedPositions, infoStorage.serveCalendarId(), location)
+		      	// .then(function (response) {
+		      	// 	// console.info(response);
+		      	// })
+		      	// .catch(function (err) {
+		      	// 	console.error(err);
+		      	// });
 		      }
 		    });
 		 
