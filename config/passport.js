ba0941b4e2af68839e2e2ca41145dbf470b8f553
Kyle Zollinger <zollingerkj@gmail.com>
2015-08-08 18:44:31 -0600

    hahaha lololol

--- config/passport.js
+++ config/passport.js
@@ -136,7 +136,7 @@ module.exports = function(passport) {
 
                                 } else if (req.body.userType.admin) {
                                     new Instructor({
-                                        userId: result._id;
+                                        userId: result._id
                                     }).save();
                                 } else if (req.body.userType.student) {
                                    // User is a new student
