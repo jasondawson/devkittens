cebff3cc407e141dfcc3b36f845de1c0dc40c19f
Andrew <andrewcrane152@gmail.com>
2015-08-10 10:31:27 -0600

    Popup created for failed login

--- public/controllers/HomeController.js
+++ public/controllers/HomeController.js
@@ -4,8 +4,13 @@ angular.module('devKittens')
 
 
 	$scope.login = function(user){
-		authService.login(user.email, user.password).then(function(response){
+		authService.login(user.email, user.password)
+		.then(function(response){
 			$location.path('/dashboard');
+		})
+		.catch(function (err) {
+			$scope.messageText = "Wrong email/password combination.";
+			return $scope.displayAlert = true;
 		})
 	}
 
