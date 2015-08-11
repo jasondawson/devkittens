aea21a0341e9623a78fe4e629126d52940cb2924
Daniel Falabella <dnlfala@gmail.com>
2015-08-10 20:01:55 -0600

    Fixed instructors' models on dashboard page.

--- public/directives/teacherModal.js
+++ public/directives/teacherModal.js
@@ -17,6 +17,7 @@ angular.module('devKittens')
 
 			$scope.toggleAddSchedule = false;
 			$scope.toggleAddPermissions = false;
+			$scope.activeTab = 'schedule';
 
 			$scope.toggleViewToSchedule = true;
 			$scope.toggleViewToPermissions = false;
@@ -45,7 +46,7 @@ angular.module('devKittens')
 				userService.instructorPermissions(obj, $scope.currentTeacher)
 				.then(function(response) {
 					$scope.messageText = "Success! " + $scope.currentTeacher.name + "'s permissions have been updated."
-					$scope.displayAlert = true;
+					$scope.displaySuccess = true;
 				})
 			}
 
